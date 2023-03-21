0ba27cf22c3cf98418e69c133fbaf29b79f8dde2
Yassine-Zaazaa <Yassine.zaazaa@outlook.com>
2023-03-21 17:10:34 +0000

    changed structs

--- philo.h
+++ philo.h
@@ -9,32 +9,43 @@
 
 typedef struct s_args
 {
-	size_t	philo_count;
+	size_t	number_of_philosophers;
 	size_t	time_to_die;
 	size_t	time_to_eat;
 	size_t	time_to_sleep;
-	size_t	max_meals;
+	size_t	max_number_of_meals;
 	int		running;
 }	t_args;
 
-typedef struct s_fork
+typedef	struct s_fork
 {
 	size_t			fork_id;
-	pthread_mutex_t	lock;
+	pthread_mutex_t	fork_mutex;
 }	t_fork;
 
-
-typedef struct s_philo
+typedef	struct s_philo
 {
 	size_t			philo_id;
 	pthread_t		thread;
-	size_t			meals_number;
-	size_t			time_left;
-	t_args			*args;
-	t_fork			*fork;
-	pthread_mutex_t	lock;
+	t_fork			*forks;
+	pthread_mutex_t	philo_mutex;
 }	t_philo;
 
+typedef	struct s_wrapper
+{
+	t_args			*args;
+	t_philo			*philos;
+	t_fork			*forks;
+	t_fork			**fork_couples;
+	pthread_mutex_t	wrapper_mutex;
+}	t_wrapper;
+
+
 t_args	*ft_parser(int ac, char **av);
+
+t_fork	*generate_forks(t_args	*args);
+t_fork	**generate_fork_couples(t_wrapper *wrapper);
+void	destroy_forks(t_wrapper *wrapper);
+void	destroy_fork_couples(t_wrapper *wrapper);
 
 #endif

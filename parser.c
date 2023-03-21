c8f138a095374fb3f7c8ed8a03a8f159dfdcdbac
Yassine-Zaazaa <Yassine.zaazaa@outlook.com>
2023-03-21 17:10:20 +0000

    parser done

--- parser.c
+++ parser.c
@@ -14,7 +14,10 @@ size_t	ft_atoi(const char *nptr)
 	if(*ptr == '-' || *ptr == '+')
 	{
 		if(*ptr == '-')
-			return (0);
+		{
+			printf("Arguments can not be negative !\n");
+			exit(0);
+		}
 		ptr++;
 	}
 	while(*ptr >= '0' && *ptr <= '9')
@@ -25,14 +28,13 @@ size_t	ft_atoi(const char *nptr)
 	return (ret);
 }
 
-
 t_args	*ft_parser(int ac, char **av)
 {
 	t_args	*args;
 
 	if(ac != 5 && ac != 6)
 	{
-		printf("Number of arguments invalid !\n");
+		printf("Arguments invalid !\n");
 		exit(0);
 	}
 	args = (t_args *)malloc(sizeof(t_args));
@@ -41,17 +43,12 @@ t_args	*ft_parser(int ac, char **av)
 		printf("Error allocating memory !\n");
 		exit(0);
 	}
-	args->philo_count = ft_atoi(av[1]);
+	args->number_of_philosophers = ft_atoi(av[1]);
 	args->time_to_die = ft_atoi(av[2]);
 	args->time_to_eat = ft_atoi(av[3]);
 	args->time_to_sleep = ft_atoi(av[4]);
 	if(ac == 6)
-		args->max_meals = ft_atoi(av[5]);
-	if(!args->philo_count || !args->time_to_die || !args->time_to_eat || !args->time_to_eat || 
-		(ac == 6 && !args->max_meals))
-	{
-		printf("Arguments invalid !\n");
-		exit(0);
-	}
+		args->max_number_of_meals = ft_atoi(av[5]);
+	args->running = 1;
 	return (args);
 }
